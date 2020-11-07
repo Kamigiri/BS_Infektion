6d3331e6c761405b92fdc059a5b3bf86fb082c90
Kamigiri <MertDemirbile@outlook.de>
2020-11-07 17:18:17 +0100

    change Object on collision

--- Assets/Script/Person.cs
+++ Assets/Script/Person.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Person : MonoBehaviour
 
 {
-    public int chance_of_infection;
+    public int chance_of_infection = 20;
     private SpriteRenderer spr;
+    public GameObject personPrefab;
+    public GameObject infectedPersonPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +26,15 @@ public class Person : MonoBehaviour
     void OnCollisionEnter2D(Collision2D coll)
     {
 
-        if (coll.gameObject.name == "person(Clone)")
+        if (coll.gameObject.name == "infectedPerson(Clone)" && this.gameObject.name == "person(Clone)")
         {
             int infection_chance = Random.RandomRange(0, 100);
 
             if (infection_chance <= chance_of_infection)
             {
-                spr.color = new Color(169, 0, 0);
+                Instantiate(infectedPersonPrefab, new Vector3(this.transform.position.x, this.transform.position.y, 0), Quaternion.identity);
+                Destroy(this.gameObject);
+
             }     
             
         }
