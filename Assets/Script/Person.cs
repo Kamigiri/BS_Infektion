0ead9f91fbcdd9cae6a9670805bf8d6223aba466
Kamigiri <MertDemirbile@outlook.de>
2020-11-07 16:59:45 +0100

    added collision

--- Assets/Script/Person.cs
+++ Assets/Script/Person.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class Person : MonoBehaviour
 
 {
-    public int i;
-    public SpriteRenderer spr;
+    public int chance_of_infection;
+    private SpriteRenderer spr;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spr = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    [System.Obsolete]
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.gameObject.name == "person(Clone)")
+        {
+            int infection_chance = Random.RandomRange(0, 100);
+
+            if (infection_chance <= chance_of_infection)
+            {
+                spr.color = new Color(169, 0, 0);
+            }     
+            
+        }
     }
 }
